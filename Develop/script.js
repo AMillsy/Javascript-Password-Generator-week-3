54b234ace6945b539af92cb977beec300886df65
AMillsy <46033976+AMillsy@users.noreply.github.com>
2023-04-13 20:04:29 +0100

    Adding all the characters into an array

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//All the characters that can be used in a password
+const characters = [
+  `1`,`2`,`3`,`4`,`5`,`6`,`7`, `8`,`9`,`0`,`q`,`w`,`e`,`r`,`t`,`y`,`u`,`i`,`o`,`p`,`a`,`s`,`d`,`f`,
+  `g`,`h`,`j`,`k`,`l`,`z`,`x`,`c`,`v`,`b`,`n`,`m`,` `,`!`,`"`,`#`,`$`,`%`,`&`,`'`,`(`,`)`,`*`,`+`,
+  `,`,`-`,`.`,`/`,`:`,`;`,`<`,`=`,`>`,`?`,`@`,`[`,`]`,'\\',`^`,`_`,"`",`{`,`|`,`}`,`~`,
+];
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Javascript-Password-Generator-week-3
+
 Making a password generator based on criteria's that the user selects
+
+## Criteria
+
+Need a password that I am presented with a series of prompts for password criteria.
