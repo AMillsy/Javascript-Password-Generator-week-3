deded4018f6c3c1eb217794bea3b207cc2c8e94d
AMillsy <46033976+AMillsy@users.noreply.github.com>
2023-04-13 20:29:26 +0100

    Making a random number generator with range

--- Develop/script.js
+++ Develop/script.js
@@ -10,6 +10,21 @@ const characters = [
 
 
 // Write password to the #password input
+function randomNumInRange(min, max){
+  //Gets the difference, that will include the max number 
+  const difference = (max - min) + 1;
+  //does a math.random which gives another between 0-1
+  //Multiply by the difference so 100 - 50 = 50; rand(0.5) * 50 = 25;  <--- Test Numbers
+  let rand = Math.floor(Math.random() * difference);
+  //25 + 50 = 75; Gives you a number in the range you specify <-- Test Numbers
+  rand += min;
+  return rand;
+}
+
+
+console.log(generatePassword());
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
